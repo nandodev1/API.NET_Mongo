c6cae0cdfeb695d321c3d5a47f94b14fd795fbe3
Fernando S. Gomes <nandobink6@gmail.com>
2021-05-11 13:35:28 -0300

    Finalização de configuração de conexão do mongoDB

--- Startup.cs
+++ Startup.cs
@@ -29,18 +29,9 @@ namespace Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Api v1"));
-            }
-
             app.UseHttpsRedirection();
 
-            app.UseRouting();
-
-            app.UseAuthorization();
+            app.UseRouting();  
 
             app.UseEndpoints(endpoints =>
             {
